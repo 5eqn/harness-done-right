b27eb5a8a4fac580e70e774f43272f94e64c5b77
5eqn <subcat2077@gmail.com>
2026-04-19 09:52:26 +0800

    Half-done academic report

---
+++ examples/academic_report/academic_report.py
@@ -0,0 +1,15 @@
+"""
+TODO implement these classes:
+
+class Reference(Task):
+    title: str
+    link: str
+    # Auto generate zotero metadata like:
+    # file (save to ~/.hdr/store/reference_validated/{filename}.pdf),
+    # author, source (conference / other), ...
+
+class 
+
+Also fulfill the core library as needed.
+
+"""
